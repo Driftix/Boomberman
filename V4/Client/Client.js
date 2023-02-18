616bb4672fe13cf84ade21dc3fe10170cd184c5b
Guilhem <xxdarkwolf26xx@gmail.com>
2023-02-18 16:28:39 +0100

    bug git ?

--- V4/Client/Client.js
+++ V4/Client/Client.js
@@ -1,10 +1,8 @@
-import { Terrain } from "./Terrain.js";
 
 // Créer une instance de WebSocket en spécifiant l'URL du serveur
 var socket = new WebSocket("ws://localhost:8001/");
 
-let terrain;
-
+let key;
 // Lorsque la connexion est établie
 socket.onopen = function() {
   // Envoyer un événement "connection" au serveur
@@ -20,72 +18,90 @@ socket.onmessage = function({data}) {
   data = JSON.parse(data)
   console.log("Reçu : " +data.event);
   switch(data.event){
-    case "explode":
-      //on récupère la partie de la carte détruite pour faire une anim à l'endroit de l'explosion
-      //puis on met à jour le visuel de la carte côté client !
-      terrain.animate(data.destroyed).then(()=>terrain.update(data.terrain));
+    //Pour join ou start on renvoie l'event join
+    case "start":
+      alert("Party Code: " +  data.key)
+      socket.send(JSON.stringify({"event":"join","key":data.key}))
+      document.getElementById("pre-game").style.display = "none";
+      document.getElementById("game-stats").style.display = "block";
+      //On met à jour la clé du jeu
+      key = data.key
       break;
     case "updateTerrain":
-      terrain.update(data.terrain);
-      break;
-    case "bombPlaced":
-      terrain.placeBomb(data.x, data.y);
-      break;
-    case "initPlayer":
-      terrain.addPlayer(data.identifier, data.x, data.y, data.playable);
-      break;
-    case "confirmMove" :
-      terrain.movePlayer(data.identifier, data.x, data.y);
-      break;
-    case "initTerrain":
-      console.log("InitTerrain...")
-      let body = document.getElementsByTagName("body")[0];
-      body.innerHTML += data.terrain
-      //On met juste en memoire le terrain envoyé par le serveur
-      //Pour pouvoir faire des actions dessus
-      terrain = new Terrain()
+      if(document.getElementById("table") == undefined){
+        //Si le terrain existe pas on l'ajoute
+        let body = document.getElementsByTagName("body")[0];
+        body.innerHTML += data.terrain
+      }else{
+        //Si le terrain existe on le met à jour
+        document.getElementById("table").remove();
+        let body = document.getElementsByTagName("body")[0];
+        body.innerHTML += data.terrain
+      }
     break;
+    case "animate":
+      data.destroyed.forEach((bloc) => {
+        try{
+            document.getElementById("table").rows[bloc[0]].cells[bloc[1]].className = "anim";
+        }catch (undefined){
+          console.log("skip")
+        }
+        
+    })
+    break;
+    case "updateBomb":
+      document.getElementById("bombquantity").textContent = data.qty;
   }
 };
 
+/*Mouvements */
 document.addEventListener("keydown", function(event) {
-  terrain.getPlayers().forEach((player) => {
-    if(player.playable){
-      let positionX = player.position[0];
-      let positionY = player.position[1];
-   
-      switch(event.key){
-        case 'z':
-          positionX--;
-          break;
-        case 's':
-          positionX ++;
-          break;
-        case 'd':
-          positionY++;
-          break;
-        case 'q':
-          positionY--;
-          break;
-        case 'f':
-            let placeBomb ={
-              "event" : "placeBomb",
-              "x" : positionX,
-              "y" : positionY,
-            }
-            socket.send(JSON.stringify(placeBomb))
-          break;
-      }
-      let move = {
-        "event" : "move",
-        "x" : positionX,
-        "y" : positionY,
-        "old_x" : player.position[0],
-        "old_y" : player.position[1], 
-      } 
-      socket.send(JSON.stringify(move))
+  console.log(key)
+  if(key != undefined){
+    let direction = "none";
+    switch(event.key){
+      case 'z':
+        direction="up"
+        break;d
+      case 's':
+        direction="down"
+        break;
+      case 'd':
+        direction="right"
+        break;
+      case 'q':
+        direction="left"
+        break;
+      case 'f':
+        let placeBomb ={
+          "event" : "placeBomb",
+        }
+        socket.send(JSON.stringify(placeBomb))
+      break;
     }
-  });
-
+    let updateMove = {
+      "event" : "move",
+      "direction" : direction,
+      "key" : key
+    }
+    socket.send(JSON.stringify(updateMove))
+  }
 });
 
+/* Gestion lancement du jeu */
+document.getElementById("createGame").addEventListener("click",()=>{
+  socket.send(JSON.stringify({
+    "event": "start",
+  }))
+})
+document.getElementById("joinGame").addEventListener("click",()=>{
+  key = document.getElementById("joinURL").value
+  socket.send(JSON.stringify({
+    "event":"join",
+    "key" : key
+  }));
+  document.getElementById("pre-game").style.display = "none";
+  document.getElementById("game-stats").style.display = "block";
+})
+
+  
